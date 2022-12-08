50053dd29dc45232336387ead66b9d2e018b79e0
jofre002 <jonas.freyer002@stud.fh-dortmund.de>
2022-12-08 20:59:29 +0100

    Interpolate Camera on Mesh change

---
+++ src/components/Control.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { OrbitControls } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { MathUtils } from 'three'
+import * as THREE from 'three'
+
+const Control = React.forwardRef(({ currentMesh }, ref) => {
+
+
+    useFrame(() => {
+        if (currentMesh !== null && currentMesh !== undefined) {
+            if (ref.current.target !== new THREE.Vector3(currentMesh.position.x, currentMesh.position.y, currentMesh.position.z)) {
+                const mesh_pos = currentMesh.position;
+                const vector = new THREE.Vector3(mesh_pos.x, mesh_pos.y, mesh_pos.z);
+                ref.current.target.x = MathUtils.lerp(ref.current.target.x, vector.x, 0.2);
+                ref.current.target.y = MathUtils.lerp(ref.current.target.y, vector.y, 0.2);
+                ref.current.target.z = MathUtils.lerp(ref.current.target.z, vector.z, 0.2);
+            }
+        }
+        else {
+            ref.current.target.x = MathUtils.lerp(ref.current.target.x, 0, 0.2);
+            ref.current.target.y = MathUtils.lerp(ref.current.target.y, 1.2, 0.2);
+            ref.current.target.z = MathUtils.lerp(ref.current.target.z, 0, 0.2);
+        }
+    });
+
+    return <OrbitControls
+        maxZoom={100}
+        minAzimuthAngle={undefined}
+        maxAzimuthAngle={undefined}
+        minPolarAngle={-Math.PI}
+        maxPolarAngle={Math.PI / 2}
+        ref={ref}
+        makeDefault={true}
+    >
+    </OrbitControls>
+})
+
+export default Control
--- src/components/WebGL.js
+++ src/components/WebGL.js
@@ -1,13 +1,15 @@
 import React, { useState, useRef, useEffect, useMemo, Suspense } from 'react';
 import uuid from 'react-uuid';
 import { Canvas, useFrame } from '@react-three/fiber';
+import { MathUtils } from 'three'
 import { useControls } from 'leva'
-import * as THREE from 'three';
+import * as THREE from 'three'
 import { OrbitControls, TransformControls, Environment, Stats, PerspectiveCamera } from '@react-three/drei';
 import { EffectComposer, DepthOfField, Bloom, Noise, Vignette, Glitch, Pixelation, SSAO, Grid, HueSaturation, ToneMapping } from '@react-three/postprocessing'
 import { BlendFunction } from 'postprocessing'
 
 import Plane from './Plane'
+import Control from './Control'
 import Model from './GLTF_Model'
 
 const WebGL = ({ shapesOnCanvas, setShapesOnCanvas, currentMesh, setCurrentMesh, groups }) => {
@@ -17,38 +19,10 @@ const WebGL = ({ shapesOnCanvas, setShapesOnCanvas, currentMesh, setCurrentMesh,
     const orbitControls = useRef();
     const transformControls = useRef();
 
-
     useEffect(() => {
         setShapesOnCanvas([...shapesOnCanvas]);
     }, [])
 
-    const setCameraTargetToMesh = () => {
-        const mesh_pos = currentMesh.position;
-        const vector = new THREE.Vector3(mesh_pos.x, mesh_pos.y, mesh_pos.z);
-        orbitControls.current.target = vector;
-    }
-
-    const resetCameraTargetToCourse = () => {
-        const vector = new THREE.Vector3(0, 1.3, 0);
-        orbitControls.current.target = vector;
-    }
-
-    useEffect(() => {
-        if (orbitControls.current !== undefined) {
-            if (currentMesh === undefined || currentMesh === null) {
-                resetCameraTargetToCourse();
-            }
-            else {
-                setCameraTargetToMesh();
-            }
-        }
-    }, [currentMesh])
-
-    useEffect(() => {
-        if (shapesOnCanvas.length === 0 && transformControls.current !== undefined) {
-            transformControls.current.detach();
-        }
-    }, [shapesOnCanvas])
 
     useEffect(() => {
         if (groups.length > 0) {
@@ -60,6 +34,7 @@ const WebGL = ({ shapesOnCanvas, setShapesOnCanvas, currentMesh, setCurrentMesh,
             }
         }
     }, [groups])
+
 
     const getMode = (e) => {
         switch (e.key) {
@@ -76,14 +51,7 @@ const WebGL = ({ shapesOnCanvas, setShapesOnCanvas, currentMesh, setCurrentMesh,
     const clickOnCanvas = (e) => {
         if (e.detail === 2) {
             transformControls.current.detach();
-            //resetCameraTargetToCourse();
             setCurrentMesh();
-        }
-    }
-
-    const mouseUp = (e) => {
-        if (currentMesh !== undefined || currentMesh !== null) {
-            setCameraTargetToMesh();
         }
     }
 
@@ -98,20 +66,12 @@ const WebGL = ({ shapesOnCanvas, setShapesOnCanvas, currentMesh, setCurrentMesh,
 
             <Plane></Plane>
             <Model receiveShadow={true} castShadow={true} key={uuid()} scale={[7, 7, 7]} filename='/course_standard.gltf'></Model>
-
             <directionalLight position-x={-100} position-y={100} position-z={-100} castShadow receiveShadow intensity={1.3} />
-
             {[...shapesOnCanvas]}
             <Environment preset="forest" background blur={0.5} />
             <PerspectiveCamera fov={70} position={[5, 5, 5]} makeDefault />
-            <OrbitControls
-                minAzimuthAngle={undefined}
-                maxAzimuthAngle={undefined}
-                minPolarAngle={-Math.PI}
-                maxPolarAngle={Math.PI / 2}
-                ref={orbitControls} makeDefault={true}
-            />
-            <TransformControls onMouseUp={(e) => mouseUp(e)} object={currentMesh} translationSnap={0.01} rotationSnap={Math.PI / 16} ref={transformControls} mode={mode} />
+            <Control ref={orbitControls} currentMesh={currentMesh} />
+            <TransformControls object={currentMesh} translationSnap={0.01} rotationSnap={Math.PI / 16} ref={transformControls} mode={mode} />
         </Canvas >
 
     );
